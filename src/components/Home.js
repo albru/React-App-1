398095c063f640270417254f95eb05941557b21d
Alexandr Bryukhanov <bryukhanovspb@gmail.com>
2018-09-21 21:18:41 +0300

    Using Axios

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,48 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import axios from 'axios'
 
-const Home = () => {
-  return (
-    <div className="container">
-      <h4 className="center">Home</h4>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam blanditiis est doloribus soluta facere? Itaque laborum, accusantium qui voluptatum odio eius officia soluta cum excepturi vero perspiciatis nemo quasi? Excepturi!</p>
-    </div>
-  )
+class Home extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      posts: [ ]
+    }
+  }
+  componentDidMount() {
+    axios.get('http://jsonplaceholder.typicode.com/posts')
+      .then(res => {
+        this.setState({
+          posts: res.data.slice(0,10)
+        })
+      })
+  }
+  render() {
+    const { posts } = this.state
+    const postList = posts.length ? (
+      posts.map(post => {
+        return (
+          <div className="post card" key={post.id}>
+            <div className="card-content">
+              <span className="card-title">
+                {post.title}
+              </span>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        )
+      })
+    ) : (
+      <div className="center">No posts yet</div>
+    )
+    return (
+      <div className="container" >
+        <h4 className="center">Home</h4>
+        {postList}
+      </div>
+    )
+  }
 }
+
 
 export default Home
